05067f001b44b163975908bef0541b9691ec350c
efradure <dureefrain@gmail.com>
2025-05-12 14:36:44 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+// Esto se ejecuta cuando la página termina de cargar
+document.addEventListener("DOMContentLoaded", function () {
+    console.log("Página cargada correctamente ✅");
+  
+
+  });
+  
